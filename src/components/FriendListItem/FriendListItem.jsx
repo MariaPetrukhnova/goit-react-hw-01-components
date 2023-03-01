a9837add104d3313fda168be604f9854cd410d5b
MariaPetrukhnova <110037119+MariaPetrukhnova@users.noreply.github.com>
2023-03-01 22:02:17 +0200

    correct errors

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,8 +1,8 @@
 import PropTypes from 'prop-types';
 import css from "./FriendListItem.module.css";
 
-export const FriendListItem = ({ avatar, name, isOnline, key }) => { 
-    return <li className={css.item} key={key}>
+export const FriendListItem = ({ avatar, name, isOnline }) => { 
+    return <li className={css.item}>
         <span className={css.status}>{isOnline}</span>
         <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
         <p className={css.name}>{name}</p>
