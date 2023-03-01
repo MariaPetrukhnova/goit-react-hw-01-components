ee44c1182a0ac5d2ab7a332392ed11b0a8ff435e
MariaPetrukhnova <110037119+MariaPetrukhnova@users.noreply.github.com>
2023-03-01 22:21:29 +0200

    online status

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -3,8 +3,10 @@ import css from "./FriendListItem.module.css";
 
 export const FriendListItem = ({ avatar, name, isOnline }) => { 
     return <li className={css.item}>
-        <span className={css.status}>{isOnline}</span>
-        <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+        <span className={`${css.status} ${isOnline ? css.statusOnline : ""}`}></span>
+        <div className={css.avatar}>
+            <img src={avatar} alt="User avatar" width="40" />
+        </div>
         <p className={css.name}>{name}</p>
     </li>
 }
